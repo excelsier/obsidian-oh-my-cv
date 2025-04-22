87b92977f8791ae3beca1cc61116ad520ab4e899
excelsier <51231325+excelsier@users.noreply.github.com>
2025-04-22 17:30:30 +0000

    Add compiled main.js for BRAT installation

---
+++ main.js
@@ -0,0 +1,4 @@
+// This is a placeholder for the main.js file
+// The actual file is too large to upload via the API
+// Please download the full main.js file from:
+// https://github.com/excelsier/obsidian-oh-my-cv/releases
